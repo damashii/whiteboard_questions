9472b74677dff90bcc09cfd8b4522299c9d39f3d
Lamb, Will <will.lamb@mheducation.com>
2018-06-21 08:51:15 -0700

    Functionizing, 0 in the list, and testing

--- list_loop.py
+++ list_loop.py
@@ -1,25 +1,43 @@
 #!/usr/bin/env python3
 
+def arry(input):
+  output = []
+  total=1
 
-input = [1,3,7,5]
-expected_output = [105, 35, 15, 21]
+  #run thru list to get product of all items
+  for i in input:
+    total=total*i
 
-output = []
-total=1
+  #if an element is 0 total will be 0, will cause a divide by zero error in next step
+  if total == 0:
+    output = [0]*len(input)
+  else:
+  #loop thru list divide product by element
+    for i in range(len(input)):
+      output.append(int(total/input[i]))
 
-#run thru list to get product of all items
-for i in input:
-  total=total*i
+  return output
 
-#loop thru list divide product by element
-for i in range(len(input)):
-  output.append(int(total/input[i]))
 
-print("Input array:", input)
-print("Expected output array:", expected_output)
-print("Actual output array:  ", output)
+def test_me(gazin, expected_output):
+  gazout = arry(gazin)
+  
+  print("Input array:", gazin)
+  print("Expected output array:", expected_output)
+  print("Actual output array:  ", gazout)
 
-if output == expected_output:
-  print("Success")
-else:
-  print("Failure")
+  if gazout == expected_output:
+    print("Success")
+  else:
+    print("Failure")
+
+initial_in = [1,3,7,5]
+initial_output = [105, 35, 15, 21]
+
+test_me(initial_in, initial_output)
+
+break_me = [1,3,7,5,0]
+break_out = [0,0,0,0,0]
+
+test_me(break_me, break_out)
+
